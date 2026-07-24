b93db0cfc4df5afee8bab2a8909230361319d596
MPinillaDavila <mpinillad97@gmail.com>
2026-07-23 19:32:17 -0500

    feat(api): version API routes

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from app.api.routes.health import router as health_router
 from app.api.routes.root import router as root_router
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(root_router)
 api_router.include_router(health_router)
